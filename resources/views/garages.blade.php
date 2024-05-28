5c3731d6997e14cc2c90701213b99d0cf590e238
HrodMarRik <somosminal@gmail.com>
2024-05-28 12:19:48 +0200

    Auto-commit from rggit

--- resources/views/garages.blade.php
+++ resources/views/garages.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Garages') }}
         </h2>
     </x-slot>
 
@@ -9,19 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @if (Route::has("garages")||Route::has("contrats")||Route::has("clients"))
-                        @if (Route::has("garages"))
-                            <a href=" {{ Route('garages') }}">mes garages</a><br>
-                        @endif
-                        @if (Route::has("clients"))
-                            <a href=" {{ Route('clients') }}">mes clients</a><br>
-                        @endif
-                        @if (Route::has("contrats"))
-                            <a href=" {{ Route('contrats') }}">mes contrats</a>
-                        @endif
-                    @else
-                        <p>pas de contenue</p>
-                    @endif
+
                 </div>
             </div>
         </div>
