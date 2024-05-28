fead220fb3996bf0088e8f9e8b04502e21cc64a1
HrodMarRik <somosminal@gmail.com>
2024-05-28 15:10:43 +0200

    Auto-commit from rggit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,9 +9,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ClientsSeeder::class,
-            GaragesSeeder::class,
-            ContratsSeeder::class,
+            ClientSeeder::class,
+            GarageSeeder::class,
+            ContratSeeder::class,
         ]);
     }
 }
