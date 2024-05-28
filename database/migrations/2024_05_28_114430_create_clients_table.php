762c781d344349240395207632ddf06b55d16ff0
HrodMarRik <somosminal@gmail.com>
2024-05-28 14:15:23 +0200

    Auto-commit from rggit

--- database/migrations/2024_05_28_114430_create_clients_table.php
+++ database/migrations/2024_05_28_114430_create_clients_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->name();
+            $table->statut();
+            $table->phone_number();
+            $table->mail();
+            $table->info();
+
         });
     }
 
