22d3a876b689f20c7ad157ed6ba1ac2f736b663f
HrodMarRik <somosminal@gmail.com>
2024-05-28 11:44:36 +0200

    Auto-commit from rggit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a>mes garages</a><br>
-                    <a>mes clients</a><br>
-                    <a>mes contrats</a>
+                    @if (Route::has("garages")||Route::has("contrats")||Route::has("clients"))
+                        @if (Route::has("garages"))
+                            <a href=" {{ Route('garages') }}">mes garages</a><br>
+                        @endif
+                        @if (Route::has("clients"))
+                            <a href=" {{ Route('clients') }}">mes clients</a><br>
+                        @endif
+                        @if (Route::has("contrats"))
+                            <a href=" {{ Route('contrats') }}">mes contrats</a>
+                        @endif
+                    @endif
                 </div>
             </div>
         </div>
