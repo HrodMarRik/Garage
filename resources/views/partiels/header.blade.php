e6c404ed8ccb7b846371f792a9f6b878782bb850
HrodMarRik <somosminal@gmail.com>
2024-05-28 17:19:12 +0200

    Auto-commit from rggit

--- resources/views/partiels/header.blade.php
+++ resources/views/partiels/header.blade.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>mes Garages</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
-        <h1>titre</h1>
+        <h1>Mon Super Site</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">À propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
     </header>
