2d5f51ca79165bec4ac477132a2ba1b5c6c81899
HrodMarRik <somosminal@gmail.com>
2024-05-28 14:05:20 +0200

    Auto-commit from rggit

--- app/Http/Controllers/GaragesController.php
+++ app/Http/Controllers/GaragesController.php
@@ -10,9 +10,10 @@ class GaragesController extends Controller
     public function show(Request $request): View {
         // aller chercher le dico garages
         $garages = "en construction";
+        $info = $request->garages();
         return view('garages', [
             'garages' => $garages,
-            'request' => $request
+            'request' => $info
         ]);
     }
 }
