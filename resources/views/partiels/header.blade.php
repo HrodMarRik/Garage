fc092c750bd19fcb792e3ee8c40bd21ec870045b
HrodMarRik <somosminal@gmail.com>
2024-05-28 17:04:07 +0200

    Auto-commit from rggit

--- resources/views/partiels/header.blade.php
+++ resources/views/partiels/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>mes Garages</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="/testlaravel/style.css">
 </head>
 <body>
     <header>
