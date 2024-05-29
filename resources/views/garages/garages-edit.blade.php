4286830aa3ff59ee5a2d2da0b5253c86fb079630
HrodMarRik <somosminal@gmail.com>
2024-05-29 17:24:32 +0200

    Auto-commit from rggit

--- resources/views/garages/garages-edit.blade.php
+++ resources/views/garages/garages-edit.blade.php
@@ -1,42 +1,72 @@
 @include('partiels.header')
 
 <div class="container">
-    <h2>Modifier le client</h2>
+    <h2>Modifier le garage</h2>
 </div>
 <div class="container">
-    <form action="{{ route('clients.update', $client->id) }}" method="POST">
+    <form action="{{ route('garages.update', $garage->id) }}" method="POST">
         @csrf
         @method('PUT')
         <table class="table table-hover">
 
             <tr>
-                <th><label for="nom">Nom</label></th>
-                <td><input type="text" name="nom" id="nom" class="form-control" value="{{ $client->nom }}" required></td>
+                <th><label for="adresse" class="form-label">Adresse</label></th>
+                <td><input type="text" name="adresse" id="adresse" class="form-control" value="{{ $garage->adresse }}" required></td>
             </tr>
 
             <tr>
-                <th><label for="statut">Statut</label></th>
+                <th><label for="montant_charges" class="form-label">Montant des Charges</label></th>
+                <td><input type="number" name="montant_charges" id="montant_charges" class="form-control" value="{{ $garage->montant_charges }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="prix_particulier" class="form-label">Prix Particulier</label></th>
+                <td><input type="number" name="prix_particulier" id="prix_particulier" class="form-control" value="{{ $garage->prix_particulier }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="prix_entreprise" class="form-label">Prix Entreprise</label></th>
+                <td><input type="number" name="prix_entreprise" id="prix_entreprise" class="form-control" value="{{ $garage->prix_entreprise }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="remboursement_emprunt_mois" class="form-label">Remboursement Emprunt par Mois</label></th>
+                <td><input type="number" name="remboursement_emprunt_mois" id="remboursement_emprunt_mois" class="form-control" value="{{ $garage->remboursement_emprunt_mois }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="date_achat" class="form-label">Date d'Achat</label></th>
+                <td><input type="date" name="date_achat" id="date_achat" class="form-control" value="{{ $garage->date_achat }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="date_fin_remboursement_emprunt" class="form-label">Date de Fin de Remboursement de l'Emprunt</label></th>
+                <td><input type="date" name="date_fin_remboursement_emprunt" id="date_fin_remboursement_emprunt" class="form-control" value="{{ $garage->date_fin_remboursement_emprunt }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="statut" class="form-label">Statut</label></th>
                 <td>
                     <select name="statut" id="statut" class="form-control" required>
-                        <option value="entreprise" {{ $client->statut == 'entreprise' ? 'selected' : '' }}>Entreprise</option>
-                        <option value="particulier" {{ $client->statut == 'particulier' ? 'selected' : '' }}>Particulier</option>
+                        <option value="entreprise" {{ $garage->statut == 'entreprise' ? 'selected' : '' }}>Entreprise</option>
+                        <option value="particulier" {{ $garage->statut == 'particulier' ? 'selected' : '' }}>Particulier</option>
                     </select>
                 </td>
             </tr>
 
             <tr>
-                <th><label for="telephone">Téléphone</label></th>
-                <td><input type="text" name="telephone" id="telephone" class="form-control" value="{{ $client->telephone }}" required></td>
+                <th><label for="telephone" class="form-label">Téléphone</label></th>
+                <td><input type="text" name="telephone" id="telephone" class="form-control" value="{{ $garage->telephone }}" required></td>
             </tr>
 
             <tr>
-                <th><label for="email">Email</label></th>
-                <td><input type="email" name="email" id="email" class="form-control" value="{{ $client->email }}" required></td>
+                <th><label for="email" class="form-label">Email</label></th>
+                <td><input type="email" name="email" id="email" class="form-control" value="{{ $garage->email }}" required></td>
             </tr>
 
             <tr>
-                <th><label for="info">Informations</label></th>
-                <td><textarea rows="5" name="info" id="info" class="form-control" required>{{ $client->info }}</textarea></td>
+                <th><label for="info" class="form-label">Informations</label></th>
+                <td><textarea rows="5" name="info" id="info" class="form-control" required>{{ $garage->info }}</textarea></td>
             </tr>
             <tr>
                 <th>Action</th>
@@ -45,7 +75,7 @@
         </table>
     </form>
 
-    <a class="btn btn-block" href="{{ route('clients.index') }}">Retour à la liste</a>
+    <a class="btn btn-block" href="{{ route('garages.index') }}">Retour à la liste</a>
 </div>
 
 @include('partiels.footer')
