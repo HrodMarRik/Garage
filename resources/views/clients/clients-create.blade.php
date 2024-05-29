1eb95d51db7c0e352e00ddece378744cc371c92d
HrodMarRik <somosminal@gmail.com>
2024-05-29 16:34:14 +0200

    Auto-commit from rggit

--- resources/views/clients/clients-create.blade.php
+++ resources/views/clients/clients-create.blade.php
@@ -2,31 +2,43 @@
 
 <div class="container">
     <h2>Créer un nouveau client</h2>
-
     <form action="{{ route('clients.store') }}" method="POST">
         @csrf
-        <div class="form-group">
-            <label for="name">Nom:</label>
-            <input type="text" id="name" name="name" class="form-control" required>
-        </div>
+        <table class="table table-hover">
+            <tr>
+                <th><label for="name">Nom:</label></th>
+                <td><input type="text" id="name" name="name" class="form-control" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="statut">Statut:</label></th>
+                <td><select id="statut" name="statut" class="form-control" required>
+                        <option value="entreprise">Entreprise</option>
+                        <option value="particulier">Particulier</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <th><label for="telephone">Téléphone:</label></th>
+                <td><input type="text" id="telephone" name="telephone" class="form-control" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="email">Email:</label></th>
+                <td><input type="email" id="email" name="email" class="form-control" required></td>
+            </tr>
+
+            <tr>
+                <th></th>
+                <td></td>
+            </tr>
+
+        </table>
 
         <div class="form-group">
-            <label for="statut">Statut:</label>
-            <select id="statut" name="statut" class="form-control" required>
-                <option value="entreprise">Entreprise</option>
-                <option value="particulier">Particulier</option>
-            </select>
-        </div>
 
-        <div class="form-group">
-            <label for="telephone">Téléphone:</label>
-            <input type="text" id="telephone" name="telephone" class="form-control" required>
-        </div>
 
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" class="form-control" required>
-        </div>
 
         <div class="form-group">
             <label for="info">Info:</label>
