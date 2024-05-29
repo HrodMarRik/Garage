a65754b98ed0ea9e97c4102f25bb57ed1c56437f
HrodMarRik <somosminal@gmail.com>
2024-05-29 09:52:29 +0200

    Auto-commit from rggit

--- resources/views/pages/clients.blade.php
+++ resources/views/pages/clients.blade.php
@@ -1,27 +1,7 @@
 @include('partiels.header')
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>nom</th>
-            <th>statut</th>
-            <th>Telephone</th>
-            <th>mail</th>
-            <th>info</th>
-            <th>action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($clients as $client)
-            <tr>
-                @foreach ($client->toArray() as $key => $value)
-                    @if ($key !== 'updated_at' && $key !== 'created_at')
-                        <td>{{ $value }}</td>
-                    @endif
-                @endforeach
-                <td><a class="btn"  href="#">modifier</a></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="container">
+    <div>
+
+    </div>
+</div>
 @include('partiels.footer')
