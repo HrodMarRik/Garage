891da98ea71bb9a567a69c805c5f99d5cfc60e48
HrodMarRik <somosminal@gmail.com>
2024-05-29 15:54:01 +0200

    Auto-commit from rggit

--- resources/views/clients/clients-show.blade.php
+++ resources/views/clients/clients-show.blade.php
@@ -4,35 +4,21 @@
     <h2>Détails du client</h2>
     <table class="table table-hover">
         <tbody>
+            @foreach ($client->toArray() as $key => $value)
+                <tr>
+                    <th>{{ $key }}</th>
+                    <td>{{ $value }}</td>
+                </tr>
+            @endforeach
             <tr>
-                <th>ID</th>
-                <td>{{ $client->id }}</td>
-            </tr>
-            <tr>
-                <th>Nom</th>
-                <td>{{ $client->name }}</td>
-            </tr>
-            <tr>
-                <th>Statut</th>
-                <td>{{ $client->statut }}</td>
-            </tr>
-            <tr>
-                <th>Téléphone</th>
-                <td>{{ $client->telephone }}</td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td>{{ $client->email }}</td>
-            </tr>
-            <tr>
-                <th>Informations</th>
-                <td>{{ $client->info }}</td>
+                <th>action</th>
+                <td><a class="btn" href="{{ route('clients.edit', $client->id) }}">Modifier</a></td>
             </tr>
         </tbody>
     </table>
     <div class="container">
 
-        <a class="btn" href="{{ route('clients.edit', $client->id) }}">Modifier</a>
+
         <form action="{{ route('clients.destroy', $client->id) }}" method="POST" style="display:inline;">
             @csrf
             @method('DELETE')
