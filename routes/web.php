0624858f474ac4095080cd21bf53d3370f7710a1
HrodMarRik <somosminal@gmail.com>
2024-05-29 11:38:06 +0200

    Auto-commit from rggit

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::middleware('auth')->group(function () {
     Route::prefix('/contrats')->name('contrats.')->group(function() {
         Route::get('/', [ContratsController::class, 'show'])->name('index');
         Route::get('-read', [ContratsController::class, 'read'])->name('read');
+        Route::get('-show', [ContratsController::class, 'show'])->name('show');
+        Route::get('-create', [ContratsController::class, 'create'])->name('create');
         Route::get('-edit', [ContratsController::class, 'edit'])->name('edit');
         Route::post('-update', [ContratsController::class, 'update'])->name('update');
         Route::delete('-delete', [ContratsController::class, 'delete'])->name('delete');
@@ -40,6 +42,8 @@ Route::middleware('auth')->group(function () {
     Route::prefix('/clients')->name('clients.')->group(function() {
         Route::get('/', [ClientsController::class, 'show'])->name('index');
         Route::get('-read', [ClientsController::class, 'read'])->name('read');
+        Route::get('-show', [ClientsController::class, 'show'])->name('show');
+        Route::get('-create', [ClientsController::class, 'create'])->name('create');
         Route::get('-edit', [ClientsController::class, 'edit'])->name('edit');
         Route::post('-update', [ClientsController::class, 'update'])->name('update');
         Route::delete('-delete', [ClientsController::class, 'delete'])->name('delete');
@@ -48,6 +52,8 @@ Route::middleware('auth')->group(function () {
     Route::prefix('/garages')->name('garages.')->group(function() {
         Route::get('/', [GaragesController::class, 'show'])->name('index');
         Route::get('-read', [GaragesController::class, 'read'])->name('read');
+        Route::get('-show', [GaragesController::class, 'show'])->name('show');
+        Route::get('-create', [GaragesController::class, 'create'])->name('create');
         Route::get('-edit', [GaragesController::class, 'edit'])->name('edit');
         Route::post('-update', [GaragesController::class, 'update'])->name('update');
         Route::delete('-delete', [GaragesController::class, 'delete'])->name('delete');
