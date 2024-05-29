8ebb65901e30f8753cb58be5d86f5dd0e66db040
HrodMarRik <somosminal@gmail.com>
2024-05-29 11:43:08 +0200

    Auto-commit from rggit

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GaragesController;
 use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\ContratsController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,34 +20,35 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::prefix('/contrats')->name('contrats.')->group(function() {
-        Route::get('/', [ContratsController::class, 'show'])->name('index');
-        Route::get('-read', [ContratsController::class, 'read'])->name('read');
-        Route::get('-show', [ContratsController::class, 'show'])->name('show');
-        Route::get('-create', [ContratsController::class, 'create'])->name('create');
-        Route::get('-edit', [ContratsController::class, 'edit'])->name('edit');
-        Route::post('-update', [ContratsController::class, 'update'])->name('update');
-        Route::delete('-delete', [ContratsController::class, 'delete'])->name('delete');
+
+    Route::prefix('contrats')->name('contrats.')->group(function() {
+        Route::get('/', [ContratsController::class, 'index'])->name('index');
+        Route::get('/{id}', [ContratsController::class, 'show'])->name('show');
+        Route::get('/create', [ContratsController::class, 'create'])->name('create');
+        Route::post('/', [ContratsController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [ContratsController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [ContratsController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ContratsController::class, 'destroy'])->name('destroy');
     });
 
-    Route::prefix('/clients')->name('clients.')->group(function() {
-        Route::get('/', [ClientsController::class, 'show'])->name('index');
-        Route::get('-read', [ClientsController::class, 'read'])->name('read');
-        Route::get('-show', [ClientsController::class, 'show'])->name('show');
-        Route::get('-create', [ClientsController::class, 'create'])->name('create');
-        Route::get('-edit', [ClientsController::class, 'edit'])->name('edit');
-        Route::post('-update', [ClientsController::class, 'update'])->name('update');
-        Route::delete('-delete', [ClientsController::class, 'delete'])->name('delete');
+    Route::prefix('clients')->name('clients.')->group(function() {
+        Route::get('/', [ClientsController::class, 'index'])->name('index');
+        Route::get('/{id}', [ClientsController::class, 'show'])->name('show');
+        Route::get('/create', [ClientsController::class, 'create'])->name('create');
+        Route::post('/', [ClientsController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [ClientsController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [ClientsController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ClientsController::class, 'destroy'])->name('destroy');
     });
 
-    Route::prefix('/garages')->name('garages.')->group(function() {
-        Route::get('/', [GaragesController::class, 'show'])->name('index');
-        Route::get('-read', [GaragesController::class, 'read'])->name('read');
-        Route::get('-show', [GaragesController::class, 'show'])->name('show');
-        Route::get('-create', [GaragesController::class, 'create'])->name('create');
-        Route::get('-edit', [GaragesController::class, 'edit'])->name('edit');
-        Route::post('-update', [GaragesController::class, 'update'])->name('update');
-        Route::delete('-delete', [GaragesController::class, 'delete'])->name('delete');
+    Route::prefix('garages')->name('garages.')->group(function() {
+        Route::get('/', [GaragesController::class, 'index'])->name('index');
+        Route::get('/{id}', [GaragesController::class, 'show'])->name('show');
+        Route::get('/create', [GaragesController::class, 'create'])->name('create');
+        Route::post('/', [GaragesController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [GaragesController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [GaragesController::class, 'update'])->name('update');
+        Route::delete('/{id}', [GaragesController::class, 'destroy'])->name('destroy');
     });
 });
 
