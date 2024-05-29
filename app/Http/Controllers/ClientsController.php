314257dab4015905af3805db6e71e7e7cf2b01c5
HrodMarRik <somosminal@gmail.com>
2024-05-29 09:32:22 +0200

    Auto-commit from rggit

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Models\Client;
 
 class ClientsController extends Controller
 {
     public function show(): View {
-        return view('pages.clients');
-    }
+        // aller chercher le dico garages
+        $clients = Garage::all();
+        return view('pages.clients', [
+            'clients' => $clients,
+        ]);
 }
