f95a918a6d0a134648d0f36c5ddd07e547fe35a1
HrodMarRik <somosminal@gmail.com>
2024-05-29 16:44:18 +0200

    Auto-commit from rggit

--- resources/views/clients/clients-create.blade.php
+++ resources/views/clients/clients-create.blade.php
@@ -6,12 +6,12 @@
         @csrf
         <table class="table table-hover">
             <tr>
-                <th><label for="name">Nom:</label></th>
+                <th><label for="name" class="form-label">Nom:</label></th>
                 <td><input type="text" id="name" name="name" class="form-control" required></td>
             </tr>
 
             <tr>
-                <th><label for="statut">Statut:</label></th>
+                <th><label for="statut" class="form-label">Statut:</label></th>
                 <td><select id="statut" name="statut" class="form-control" required>
                         <option value="entreprise">Entreprise</option>
                         <option value="particulier">Particulier</option>
@@ -20,17 +20,17 @@
             </tr>
 
             <tr>
-                <th><label for="telephone">Téléphone:</label></th>
+                <th><label for="telephone" class="form-label">Téléphone:</label></th>
                 <td><input type="text" id="telephone" name="telephone" class="form-control" required></td>
             </tr>
 
             <tr>
-                <th><label for="email">Email:</label></th>
+                <th><label for="email" class="form-label">Email:</label></th>
                 <td><input type="email" id="email" name="email" class="form-control" required></td>
             </tr>
 
             <tr>
-                <th><label for="info">Info:</label></th>
+                <th><label for="info" class="form-label">Info:</label></th>
                 <td><textarea id="info" name="info" class="form-control" rows="3" required></textarea></td>
             </tr>
 
