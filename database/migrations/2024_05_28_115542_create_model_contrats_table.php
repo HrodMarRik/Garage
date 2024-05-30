aebafc0d0621c1feae047f504fae0cbdeae1dbc8
HrodMarRik <somosminal@gmail.com>
2024-05-30 11:05:30 +0200

    Auto-commit from rggit

--- database/migrations/2024_05_28_115542_create_model_contrats_table.php
+++ database/migrations/2024_05_28_115542_create_model_contrats_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('model_contrats', function (Blueprint $table) {
+        Schema::create('modelcontrats', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('status');
             $table->text('structure');
             $table->timestamps();
         });
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('model_contrats');
+        Schema::dropIfExists('modelcontrats');
     }
 };
