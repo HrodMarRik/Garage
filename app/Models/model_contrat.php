538d4338502ccef8287d2d8b674c4e5bafce74df
HrodMarRik <somosminal@gmail.com>
2024-05-30 09:36:38 +0200

    Auto-commit from rggit

--- app/Models/model_contrat.php
+++ app/Models/model_contrat.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class model_contrat extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'status',
+        'structure',
+    ];
 }
--- resources/views/contrats/contrats-index.blade.php
+++ resources/views/contrats/contrats-index.blade.php
@@ -1,27 +1,28 @@
 @include('partiels.header')
-
 <div class="container">
-    <a href="{{ route('contrats.create') }}" class="btn btn-block"><h2>Nouveau</h2></a>
-
+    <h2>Liste des Contrats</h2>
+</div>
+<div class="container">
+    <a href="{{ route('contrats.create') }}" class="btn btn-block">Créer un nouveau contrat</a>
     <table class="table table-hover">
         <thead>
             <tr>
-                <th>id</th>
-                <th>id_client</th>
-                <th>id_garage</th>
-                <th>Statut</th>
-                <th>structure</th>
-                <th>action</th>
+                <th>ID</th>
+                <th>Client</th>
+                <th>Garage</th>
+                <th>Status</th>
+                <th>Structure</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($contrats as $contrat)
+            @foreach($contrats as $contrat)
                 <tr>
-                    @foreach ($contrat->toArray() as $key => $value)
-                        @if ($key !== 'updated_at' && $key !== 'created_at')
-                            <td>{{ $value }}</td>
-                        @endif
-                    @endforeach
+                    <td>{{ $contrat->id }}</td>
+                    <td>{{ $contrat->client->name }}</td>
+                    <td>{{ $contrat->garage->adresse }}</td>
+                    <td>{{ $contrat->status }}</td>
+                    <td>{{ $contrat->structure }}</td>
                     <td>
                         <div>
                             <a class="btn" href="{{ route('contrats.show', $contrat->id) }}">Afficher</a>
