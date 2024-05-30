3f51dbf3df462b5ab87d5955db22f5b6fa6bb88c
HrodMarRik <somosminal@gmail.com>
2024-05-30 09:46:41 +0200

    Auto-commit from rggit

---
+++ resources/views/model_contrat/model_contrats-create.blade.php
@@ -0,0 +1,55 @@
+@include('partiels.header')
+
+<div class="container">
+    <h2>Modifier le contrat</h2>
+    <form action="{{ route('contrats.update', $contrat->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <table class="table table-hover">
+            <tr>
+                <th><label for="id_client" class="form-label">Client:</label></th>
+                <td>
+                    <select id="id_client" name="id_client" class="form-control" required>
+                        @foreach($clients as $client)
+                            <option {{ $contrat->id_client == $client->id ? 'selected' : '' }}>
+                                {{ $client->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <th><label for="id_garage" class="form-label">Garage:</label></th>
+                <td>
+                    <select id="id_garage" name="id_garage" class="form-control" required>
+                        @foreach($garages as $garage)
+                            <option {{ $contrat->id_garage == $garage->id ? 'selected' : '' }}>
+                                {{ $garage->adresse }}
+                            </option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <th><label for="status" class="form-label">Statut:</label></th>
+                <td><input type="text" id="status" name="status" class="form-control" value="{{ $contrat->status }}" required></td>
+            </tr>
+
+            <tr>
+                <th><label for="structure" class="form-label">Structure:</label></th>
+                <td><input type="text" id="structure" name="structure" class="form-control" value="{{ $contrat->structure }}" required></td>
+            </tr>
+
+            <tr>
+                <th>Action:</th>
+                <td><button class="btn btn-block" type="submit">Enregistrer les modifications</button></td>
+            </tr>
+        </table>
+    </form>
+
+    <a class="btn btn-block" href="{{ route('contrats.index') }}">Retour à la liste</a>
+</div>
+
+@include('partiels.footer')
